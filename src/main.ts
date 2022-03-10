c27f0eab394f4eb668035ceb44902bc56d85f424
Aaron Dewes <aaron.dewes@protonmail.com>
2022-03-10 16:21:56 +0100

    Minor improvements

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-figma.showUI(__html__, { width: 300, height: 400 })
+figma.showUI(__html__, { width: 300, height: 400 });
 
-figma.ui.onmessage = message => {
-  let icon: FrameNode;
-  icon = figma.createNodeFromSvg(
+figma.ui.onmessage = (message: { type: string; svg: string; }) => {
+  let iconFrame: FrameNode;
+  iconFrame = figma.createNodeFromSvg(
     message.svg
   );
-  console.log(icon.children);
+  let icon = figma.group([...iconFrame.children], figma.currentPage);
+  iconFrame.remove();
   icon.name = message.type
   icon.x = figma.viewport.center.x
   icon.y = figma.viewport.center.y
+  icon.constrainProportions = true
   figma.currentPage.selection = [icon]
 }
